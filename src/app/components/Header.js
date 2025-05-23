e70290a18d5004b664523c912461b840c8371d78
Julian <zS22017046@estudiantes.uv.mx>
2025-05-22 21:24:46 -0600

    add: nav bar

---
+++ src/app/components/Header.js
@@ -0,0 +1,30 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <a href="/">
+        <div className={styles.imgContainer}>
+          <img src="/img/logo_acopiatech.png" alt="" className={styles.img} />
+        </div>
+      </a>
+      <ul className={styles.navList}>
+        <li>
+          <a href="/sostenibilidad" className={styles.item}>
+            Sostenibilidad
+          </a>
+        </li>
+        <li>
+          <a href="/recoleccion" className={styles.item}>
+            Recolección
+          </a>
+        </li>
+        <li>
+          <a href="/nosotros" className={styles.item}>
+            Nosotros
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1>Hola perros</h1>
+    <section>
+      <h1>Hola perros</h1>
+    </section>
   );
 }
+
