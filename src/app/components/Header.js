170b8ee19b2304debaa3369c981fcd385bff1c40
Julian <zS22017046@estudiantes.uv.mx>
2025-05-25 22:02:08 -0600

    add: nav bar toggle

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,7 +1,16 @@
+"use client"
+
 import styles from "./Header.module.css";
 import { RUBIK_SANS } from "../utils/constants";
+import { useState } from "react";
 
 export default function Header() {
+  const [visible, setVisible] = useState(false);
+
+  const changeVisibility = () => {
+    setVisible(!visible);
+  };
+
   return (
     <header className={`${styles.headerContainer} ${RUBIK_SANS}`}>
       <a href="home">
@@ -9,23 +18,32 @@ export default function Header() {
           <img src="img/logo_acopiatech.png" alt="" className={styles.img} />
         </div>
       </a>
-      <ul className={styles.navList}>
-        <li>
-          <a href="servicios" className={styles.item}>
-            Servicios
-          </a>
-        </li>
-        <li>
-          <a href="sostenibilidad" className={styles.item}>
-            Sostenibilidad
-          </a>
-        </li>
-        <li>
-          <a href="nosotros" className={styles.item}>
-            Nosotros
-          </a>
-        </li>
-      </ul>
+
+      <button
+        className={styles.mobileNavToggle}
+        onClick={changeVisibility}
+        data-visible={visible}
+      ></button>
+
+      <nav>
+        <ul className={styles.navList} data-visible={visible}>
+          <li>
+            <a href="servicios" className={styles.item}>
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a href="sostenibilidad" className={styles.item}>
+              Sostenibilidad
+            </a>
+          </li>
+          <li>
+            <a href="nosotros" className={styles.item}>
+              Nosotros
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
